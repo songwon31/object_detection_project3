64fc9370e12735338767ea13b35b056d2ce64c2d
haeryong <nabi4622@snu.ac.kr>
2023-01-19 14:42:46 +0900

    change traffic light recog algorithm

--- is_green.cpp
+++ is_green.cpp
@@ -1,55 +1,67 @@
-bool LaneKeepingSystem::is_green_light()
+void traffic_sign_recognition()
 {
-  cv::Mat traffic_light = frame_(cv::Range(box_xmin, box_xmax), cv::Range(box_ymin, box_ymax));
+  int width = box_xmax - box_xmin;
+  int height = box_ymax - box_ymin;
+  cv::Mat traffic_light_upside = frame_(cv::Rect(0, 0, width, height/3));
+  cv::Mat traffic_light_downside = frame_(cv::Rect(0, height * 2 / 3, width, height/3));
+  
+  cv::Mat hsv_upside, hsv_downside;
+  std::vector<cv::Mat> hsv_upside_split, hsv_downside_split;
+  
+  cv::cvtColor(traffic_light_upside, hsv_upside, cv::COLOR_BGR2HSV);
+  cv::cvtColor(traffic_light_downside, hsv_downside, cv::COLOR_BGR2HSV);
 
-  // cv::Mat traffic_light_upside = frame_(cv::Range(box_xmin, box_xmax), cv::Range(box_ymin, box_ymin + (box_ymax -
-  // box_ymin) / 3)); cv::Mat traffic_light_downside = frame_(cv::Range(box_xmin, box_xmax), cv::Range(box_ymin +
-  // (box_ymax - box_ymin) *  2 / 3, box_ymax));
+  cv::split(hsv_upside, hsv_upside_split);
+  cv::split(hsv_downside, hsv_downside_split);
 
-  // cv::Mat hsv_upside, hsv_downside;
-  // cv::cvtColor(traffic_light_upside, hsv_upside, cv::COLOR_BGR2HSV);
+  float value_upside, value_downside;
+  int w = traffic_light_upside.size().width;
+  int h = traffic_light_upside.size().height;
 
-  cv::Mat hsv;
-  cv::cvtColor(traffic_light, hsv, cv::COLOR_BGR2HSV);
+  value_upside = cv::mean(hsv_upside_split[2](cv::Rect(w / 4, h / 4, w / 2, h / 2)))[0];
+  value_downside = cv::mean(hsv_downside_split[2](cv::Rect(w / 4, h / 4, w / 2, h / 2)))[0];
 
-  std::vector<cv::Mat> planes;
-  cv::split(hsv, planes);
-
-  cv::Mat h, v = planes[0], planes[2];
-
-  std::vector<cv::Vec3f> circles;
-  cv::HoughCircles(v, circles, cv::HOUGH_GRADIENT_ALT, 1.5, 10, 300, 0.9, 20, 50);
-
-  int max_idx(-1);
-  float max_val(0);
-  cv::Rect max_rect;
-
-  for (int i = 0; i < circles.size(); i++)
+  if(value_upside < 120 && value_downside < 120)
   {
-    cv::Vec3i c = circles[i];
-
-    cv::Rect rect(c[0] - c[2], c[1] - c[2], c[0] + c[2], c[1] + c[2]);
-
-    float mean_v = cv::mean(v(rect))[0];
-    if (mean_v > max_val)
-    {
-      max_idx = i;
-      max_val = mean_v;
-      max_rect = rect;
-    }
+    std::cout << "traffic light all off" << std::endl;
+    std::cout << "v_upside : " << value_upside << "v_downside : " << value_downside << std::endl;
+    
+    return;
   }
 
-  h += 30;
+  cv::Mat non_target;
 
-  float mean_hue = cv::mean(h[max_rect]);
-  if(mean_hue > 70 && mean_hue < 150)
+  if (value_upside > value_downside)
   {
-    return true;
+    non_target = hsv_downside_split[0];
   }
-  else if(mean_hue > 0 && mean_hue < 60 )
+  else
   {
-    return false
+    non_target = hsv_upside_split[0];
   }
 
-  return false;
+
+  cv::Mat red_mask, green_mask;
+  cv::Scalar red_lower(140), red_upper(180);
+  cv::Scalar green_lower(50), green_upper(90);
+
+  cv::inRange(non_target, red_lower, red_upper, red_mask);
+  cv::inRange(non_target, green_lower, green_upper, green_mask);
+
+  int red_pixels = cv::countNonZero(red_mask);
+  int green_pixels = cv::countNonZero(green_mask);
+
+
+  // edit here --------------------------------------------------------------------
+  if(red_pixels > green_pixels)
+  {
+    std::cout << "green light!" << std::endl;
+    return;
+  }
+  else
+  {
+    std::cout << "red light!" << std::endl;
+    return;
+  }
+
 }
